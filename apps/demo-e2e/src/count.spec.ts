05c9eb4803b309af6b56b3ba33284e994c9e9247
Mark Thompson <mthompson@bitovi.com>
2025-04-10 13:21:43 -0700

    testing if single test block works

--- apps/demo-e2e/src/count.spec.ts
+++ apps/demo-e2e/src/count.spec.ts
@@ -6,6 +6,18 @@ test('has count title 0', async ({ page }) => {
   const innerText = await page.locator('h1.header').innerText();
   console.log('innerText', innerText);
   expect(innerText).toContain('0');
+
+  await page.goto('/count/1');
+
+  const innerText2 = await page.locator('h1.header').innerText();
+  console.log('innerText2', innerText);
+  expect(innerText2).toContain('1');
+
+  await page.goto('/count/1');
+
+  const innerText3 = await page.locator('h1.header').innerText();
+  console.log('innerText3', innerText);
+  expect(innerText3).toContain('2');
 });
 
 test('has count title 1', async ({ page }) => {
