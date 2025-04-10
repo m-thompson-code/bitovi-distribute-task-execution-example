d39d2ea89b8fb4d4a8c3f9821487dce9ca7a1884
Mark Thompson <mthompson@bitovi.com>
2025-04-10 13:13:46 -0700

    updated to have logs

--- apps/demo-e2e/src/count.spec.ts
+++ apps/demo-e2e/src/count.spec.ts
@@ -3,29 +3,39 @@ import { test, expect } from '@playwright/test';
 test('has count title 0', async ({ page }) => {
   await page.goto('/count/0');
 
-  expect(await page.locator('h1.header').innerText()).toContain('The current number is 0');
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('innerText', innerText);
+  expect(innerText).toContain('0');
 });
 
 test('has count title 1', async ({ page }) => {
   await page.goto('/count/1');
 
-  expect(await page.locator('h1.header').innerText()).toContain('The current number is 1');
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('innerText', innerText);
+  expect(innerText).toContain('1');
 });
 
 test('has count title 2', async ({ page }) => {
   await page.goto('/count/2');
 
-  expect(await page.locator('h1.header').innerText()).toContain('The current number is 2');
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('innerText', innerText);
+  expect(innerText).toContain('2');
 });
 
 test('has count title 3', async ({ page }) => {
   await page.goto('/count/3');
 
-  expect(await page.locator('h1.header').innerText()).toContain('The current number is 3');
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('innerText', innerText);
+  expect(innerText).toContain('3');
 });
 
 test('has count title 4', async ({ page }) => {
   await page.goto('/count/4');
 
-  expect(await page.locator('h1.header').innerText()).toContain('The current number is 4');
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('innerText', innerText);
+  expect(innerText).toContain('4');
 });
