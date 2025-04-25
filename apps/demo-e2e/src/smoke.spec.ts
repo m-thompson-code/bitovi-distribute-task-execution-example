9c55ccef21aa9ed7e54f3a458b413c653e190ed8
Mark Thompson <mthompson@bitovi.com>
2025-04-25 16:24:02 -0700

    smoke test

---
+++ apps/demo-e2e/src/smoke.spec.ts
@@ -0,0 +1,10 @@
+// Generated file
+import { test, expect } from '@playwright/test';
+
+test('has count title 0', async ({ page }) => {
+  await page.goto('/0');
+
+  const innerText = await page.locator('h1.header').innerText();
+  console.log('smoke test');
+  expect(innerText).toContain('0');
+});
